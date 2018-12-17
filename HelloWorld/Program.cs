4e9f2a4a396e2a0f2e70866885b0b152fd6a44fc
THOMAS FERNEZ <TFE15965@open-groupe.com>
2018-12-17 13:57:20 +0100

    ex 1.4

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,25 +8,29 @@ namespace HelloWorld
         {
             string nom;
             string prenom;
+            string[] fullname = new string[2];
 
             Console.WriteLine("Entrez votre nom: ");
             nom = Console.ReadLine();
+            fullname[0] = nom;
+           
 
-            if (String.IsNullOrWhiteSpace(nom))
+            if (String.IsNullOrWhiteSpace(fullname[0]))
             {
                 Console.WriteLine("Le nom n'a pas été renseigné");
             }
 
             Console.WriteLine("Entrez votre prenom: ");
             prenom = Console.ReadLine();
+            fullname[1] = prenom;
 
-            if (String.IsNullOrWhiteSpace(prenom))
+            if (String.IsNullOrWhiteSpace(fullname[1]))
             {
                 Console.WriteLine("Le prénom n'a pas été renseigné");
             }
             else
             {
-                Console.WriteLine($"Prenom: {prenom} Nom: {nom}");
+                Console.WriteLine(fullname[0] + fullname[1]);
             }
 
             Console.ReadLine();
